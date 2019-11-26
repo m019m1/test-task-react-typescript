ac35675a56da31dac90fb5a00605b4f7f47054de
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-11-26 15:02:12 +0700

    added Enter press handle to the form

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -10,9 +10,12 @@ type Props = {
 }
 
 const Form: React.FC<Props> = ({value, onChange, placeholder, onClick, btnValue}) => {
+	const go = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		event.key === 'Enter' && onClick();
+	}
 	return (
 		<div className="form">
-			<input className='form__input' value={value} onChange={onChange} type="text" placeholder={placeholder} />
+			<input className='form__input' value={value} onChange={onChange} type="text" placeholder={placeholder} onKeyUp={go}/>
 			<button className='form__submit' onClick={onClick} disabled={value ? false : true}> {btnValue} </button>
 		</div>
 	)
